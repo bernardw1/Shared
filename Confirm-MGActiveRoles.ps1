464b25e3609342e55470c90f34a390184bab224e
bernardw1 <welmers@gmail.com>
2024-02-01 16:03:05 -0600

    Create Confirm-MGActiveRoles.ps1
    This function returns true or false depending on if the user role has been assigned and is active.

---
+++ Confirm-MGActiveRoles.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+function to check if a PIM role is currently assigned for the current user.
+
+This is a great check to use for scripts that use Mircrosoft Graph in PIM environments so that the script can error out before trying to run commands
+
+.NOTES
+Created by Bernard on 2/1/2024
+#>
+
+
+Function Confirm-MGActiveRoles ($RoleName){
+
+    #Make sure that there is a connection Microsoft Graph
+    Connect-MgGraph -UseDeviceAuthentication -NoWelcome
+    
+    #Get the current user
+    $context = Get-MgContext
+    $currentUser = (Get-MgUser -UserId $context.Account).Id
+
+    #Get current active roles
+    $CurrentRoles = Get-MgRoleManagementDirectoryRoleAssignment -Filter "principalId eq '$currentUser'" -ExpandProperty roledefinition
+
+    $isactive = 0
+
+    #check if role of interest is in the list of active roles
+    ForEach ($CurrentRole in $CurrentRoles){
+        if ($currentrole.RoleDefinition.DisplayName -eq "'$rolename'"){
+            $isactive ++
+        }
+
+    }
+    #return true or false as to if the role is active
+    if ($isactive -gt 0){
+        Write-Output "1"
+    }else{
+        Write-Output "0"
+    }
+}
